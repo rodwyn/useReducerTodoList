84fe5868368e28de0012d18ab3024bd2564b5d8f
rodwyn <rodwynmoreno@gmail.com>
2021-10-08 18:05:12 -0500

    Added app markup structure.

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -4,7 +4,7 @@ import "./App.css";
 
 const initialState = [
   {
-    id: new Date().getTime,
+    id: new Date().getTime(),
     desc: "Aprender React",
     done: false
   }
@@ -15,13 +15,35 @@ export const TodoApp = () => {
   console.log(todos);
   return (
     <div className="App">
-      <h1>Todo App</h1>
+      <h1>Todo App <small>({todos.length})</small></h1>
       <hr />
-      <ul>
-        <li>a</li>
-        <li>b</li>
-        <li>c</li>
-      </ul>
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+          {
+            todos.map((todo, i) => (
+              <li
+              className="list-group-item"
+              key={todo.id}>
+                <p className="text-center">{i + 1}.{todo.desc}</p>
+                <button className="btn btn-danger">Delete</button>
+              </li>
+              
+            ))
+          }
+        </ul>
+        </div>
+        <div className="col-5">
+          <h4>Add ToDo</h4>
+          <hr />
+          <form>
+            <input type="text" name="Desciption" placeholder="Add Todo" autoComplete="off" className="form-control" />
+            <div class="d-grid gap-2 mt-2">
+              <button className="btn btn-outline-primary">Add</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
