a1403ee59a41a65bebdf0ef8614c7651fac742dd
rodwyn <rodwynmoreno@gmail.com>
2021-10-08 18:37:41 -0500

    Added action to add Todos

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -11,8 +11,23 @@ const initialState = [
 ];
 
 export const TodoApp = () => {
-  const [todos] = useReducer(todoReducer, initialState);
-  console.log(todos);
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      desc: "Nueva tarea",
+      done: false
+    };
+
+    const action = {
+      type: 'add',
+      payload: newTodo
+    };
+
+    dispatch(action);
+  };
   return (
     <div className="App">
       <h1>Todo App <small>({todos.length})</small></h1>
@@ -36,10 +51,10 @@ export const TodoApp = () => {
         <div className="col-5">
           <h4>Add ToDo</h4>
           <hr />
-          <form>
+          <form onSubmit={ handleSubmit }>
             <input type="text" name="Desciption" placeholder="Add Todo" autoComplete="off" className="form-control" />
             <div class="d-grid gap-2 mt-2">
-              <button className="btn btn-outline-primary">Add</button>
+              <button className="btn btn-outline-primary" type="submit">Add</button>
             </div>
           </form>
         </div>
--- src/todoReducer.js
+++ src/todoReducer.js
@@ -1,8 +1,8 @@
 export const todoReducer = (state = [], action) => {
   switch (action.type) {
-    /*case 'add':
+    case 'add':
       
-      break;*/
+      return [...state, action.payload];
 
     default:
       return state;
